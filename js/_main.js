eaeee76d80eeb2d239e452ca8f3d506d0e2064b6
wat ikuka <rwwwtx@gmail.com>
2017-05-12 11:23:42 +0900

    make memo

--- js/_main.js
+++ js/_main.js
@@ -1,5 +1,14 @@
 //クラスを使って書き直し
 
+function Record(){
+    for(let x = 1; x <= 9 ;x++){
+        this.cell[x] = [];
+    for(let y = 1; y <= 9 ;y++){
+        this.cell[x][y] = 0;
+    }
+    }
+}
+
 function Board(){
     this.sente = true;
     this.cell = [];
@@ -7,15 +16,9 @@ function Board(){
     this.init();
     this.buildBoard();
 }
-Board.prototype.firstProcedure = function(){
 
-};
-Board.prototype = {
-    banInit: function(){
-        while(ban.firstChild){
-          ban.removeChild(ban.firstChild);
-        }
-    },
+Board.prototype.init = function(){
+    //9 * 9マスを作り、変数cellに入れる。
     ban:            document.getElementById("ban"),
     sente_komadai:  document.getElementById("senteKomadai"),
     gote_komadai:   document.getElementById("goteKomadai"),
@@ -23,19 +26,10 @@ Board.prototype = {
     goteText:       document.getElementById("goteText"),
     EMPTY:          0,
 };
-
-Board.prototype.init = function(){
-    //9 * 9マスを作り、変数cellに入れる。
-    for(let x = 1; x <= 9 ;x++){
-        this.cell[x] = [];
-    for(let y = 1; y <= 9 ;y++){
-        this.cell[x][y] = this.EMPTY;
-    }
-    }
-    return this.cell;
-};
 Board.prototype.buildBoard = function(){
-    this.banInit();
+    while(ban.firstChild){
+      ban.removeChild(ban.firstChild);
+    }
     //cellの情報をコピーして、それに応じた盤を生成する。
     var empty = document.getElementById("square");
     var tmp = document.createDocumentFragment();
@@ -48,14 +42,14 @@ Board.prototype.buildBoard = function(){
     }
     }
     ban.appendChild(tmp);
-  };
-
+};
+Board.prototype.generateKoma = function(){
+    var
+}
 Board.prototype.changeSente = function(){
     this.sente = !this.sente;
 };
 var board = new Board();
-
-
 
 
 function Koma(name, member, position_x, position_y){
@@ -70,47 +64,19 @@ Koma.prototype.getPicture = function(){
     var id = this.member === "sente" ? this.name : "_" + this.name;
     this.picture = document.getElementById(id);
 };
-Koma.prototype.append = function(){
+Koma.prototype.appendChild = function(){
 
 };
-var Fu1 = new Koma("koma","sente",3,3);
-// Koma.prototype.name = {
-//     this.EMPTY =          0;
-//
-//     this.FU    =          1;
-//     this.TO    =          2;
-//     this.KYO   =          3;
-//     this.NKYO  =          4;
-//     this.KEI   =          5;
-//     this.NKEI  =          6;
-//     this.GIN   =          7;
-//     this.NGIN  =          8;
-//     this.KIN   =          9;
-//     this.HI    =         10;
-//     this.RYU   =         11;
-//     this.KAKU  =         12;
-//     this.UMA   =         13;
-//     this.GYOKU =         14;
-//
-//     this.FU_   =         15;
-//     this.TO_   =         16;
-//     this.KYO_  =         17;
-//     this.NKYO_ =         18;
-//     this.KEI_  =         19;
-//     this.NKEI_ =         20;
-//     this.GIN_  =         21;
-//     this.NGIN_ =         22;
-//     this.KIN_  =         23;
-//     this.HI_   =         24;
-//     this.RYU_  =         25;
-//     this.KAKU_ =         26;
-//     this.UMA_  =         27;
-//     this.GYOKU_=         28;
-//
-//     this.senteMin =       1;
-//     this.senteMax =      14;
-//     this.goteMin  =      15;
-//     this.goteMax  =      28;
-//     this.promote  =       1;   //歩、香、桂、銀、角、飛車 + 1 === 成り駒。
-//     this.narigoma = [2,4,6,8,11,13,16,18,20,22,25,27];
-// };
+for(){
+    var "fu" + i = new Koma();
+}
+var Fu = new Koma();
+Fu.prototype.move (){
+
+}
+var Fu2;
+var Fu3;
+var Fu4;
+var Fu5;
+
+var game = new Shogi_game();
--- js/memo.php
+++ js/memo.php
@@ -1,14 +1,14 @@
 <?php
 boardの機能
-盤の作成、先手・後手の管理
-住所としての情報
+盤の作成、先手・後手の管理buildboard,sente
+住所としての情報cell[]
 
 komadaiの機能
 先手・後手のふたつある
 
 recordの機能
 どこの駒がどこに移動したかの情報を持つ。
-いずれは寄付に対応できるようにしたい。k
+いずれは寄付に対応できるようにしたい。
 
 komaの機能
 どこにいるか把握する
@@ -18,6 +18,37 @@ komaの機能
 移動する(駒台に)
 
 駒ごとの情報
-画像をもつ
+-- 画像をもつ --
 どこに移動できるか、の情報をもつ
 誰が所持しているか
+移動
+position-x,yの更新 + cellにもその情報を送る。
+現在地position-x,position-y,
+
+
+完成系
+btn.onclick = new ShogiGame(board,koma,record);で全てが完了する。
+部品
+boardとkoma,あとはrecord。
+
+先手・後手の管理
+駒を動かす(盤上・駒台の上)
+記録
+
+imformation:盤上の情報管理sente,gote,どこになんの駒がいるか
+Board:htmlへの代入
+koma:senteの駒か、goteの駒か、
+
+
+// var game =  new shogiGame("imfo", "board", "koma");
+shogiGame = function(){
+
+    imfo : 
+    board: html描画・管理sente gote
+    koma:
+}
+game
+sente gote
+現在どうなってる
+記録
+ルール？
