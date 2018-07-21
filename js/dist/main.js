b736b75aeb30ae5f9095dfee8c753028eb0baafd
kazuki watanabe <kazukiwatanabe@kazuki-no-iMac.local>
2018-07-21 22:57:43 +0900

    update modules

--- js/dist/main.js
+++ js/dist/main.js
@@ -1,4 +1,4 @@
-(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 const rule = require('./rule');
 
 function Board() {
@@ -204,12 +204,9 @@ Board.prototype.render = function () {
                 top = 0;
             if (index < 3) {
                 left = this.m.PIECE_WIDTH * index;
-                console.log(left);
             } else if (index < 6) {
                 left = this.m.PIECE_WIDTH * (index - 3);
                 top = this.m.PIECE_HEIGHT * 1;
-                console.log(left);
-                console.log(top);
             } else {
                 left = this.m.PIECE_WIDTH * (index - 6);
                 top = this.m.PIECE_HEIGHT * 2;
@@ -246,7 +243,6 @@ Board.prototype.render = function () {
             //  todo: +15されているのは変なので構成を変える
 
             const piece_type = String(this.stat.piecestand_gote[index].type - 15);
-            console.log(piece_type);
             //  駒の画像のノードを作成
             let piece = this.m.loader.getResult(piece_type).cloneNode(false);
             piece.classList.add('koma', 'gote');
