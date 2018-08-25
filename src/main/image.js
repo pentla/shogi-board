f99b25d33ca399cd7bf6edc1dcd84fca7969157c
watkazuki <watkazuki03@gmail.com>
2018-08-25 23:22:49 +0900

    画像をimportできるように

--- src/main/image.js
+++ src/main/image.js
@@ -1,6 +1,17 @@
-const img = {};
 import Fu from '../assets/img/fu.png';
-import Fu_ from '../assets/img/fu_.png';
+import Kei from '../assets/img/kei.png';
+import nKei from '../assets/img/nkei.png';
+import Kyo from '../assets/img/kyo.png';
+import nKyo from '../assets/img/nkyo.png';
 import Gin from '../assets/img/gin.png';
+import nGin from '../assets/img/ngin.png';
+import Kin from '../assets/img/kin.png';
+import Hisya from '../assets/img/hi.png';
+import Ryu from '../assets/img/ryu.png';
+import Kaku from '../assets/img/kaku.png';
+import Uma from '../assets/img/uma.png';
+import Gyoku from '../assets/img/ou.png';
 
-export default img;
+export default {
+  Fu, Kei, nKei, Kyo, nKyo, Gin, nGin, Kin, Hisha, Ryu, Kaku, Uma, Gyoku
+};
