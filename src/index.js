2eb73b8cc42ff422652449cc40c91f2abb252a36
kazuki watanabe <kazukiwatanabe@kazuki-no-iMac.local>
2018-09-02 19:55:25 +0900

    file-loaderによる画像のインポート

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
+<<<<<<< Updated upstream
+=======
+import img from './main/image';
+>>>>>>> Stashed changes
 import './assets/css/style.css';
--- src/main/image.js
+++ src/main/image.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import fu from '../assets/img/fu.png';
 import kei from '../assets/img/kei.png';
 import nkei from '../assets/img/nkei.png';
@@ -14,4 +15,22 @@ import gyoku from '../assets/img/ou.png';
 
 export default {
   Fu, kei, nkei, kyo, nkyo, gin, ngin, kin, hisha, ryu, kaku, uma, gyoku
+=======
+import Fu from '../assets/img/fu.png';
+import Kei from '../assets/img/kei.png';
+import nKei from '../assets/img/nkei.png';
+import Kyo from '../assets/img/kyo.png';
+import nKyo from '../assets/img/nkyo.png';
+import Gin from '../assets/img/gin.png';
+import nGin from '../assets/img/ngin.png';
+import Kin from '../assets/img/kin.png';
+import Hisya from '../assets/img/hi.png';
+import Ryu from '../assets/img/ryu.png';
+import Kaku from '../assets/img/kaku.png';
+import Uma from '../assets/img/uma.png';
+import Gyoku from '../assets/img/ou.png';
+
+export default {
+  Fu, Kei, nKei, Kyo, nKyo, Gin, nGin, Kin, Hisha, Ryu, Kaku, Uma, Gyoku
+>>>>>>> Stashed changes
 };
