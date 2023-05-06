8cc8f0c0abae24da2390421d2bb9292fff9a0980
pentla <watkazuki03@gmail.com>
2023-05-06 20:29:57 +0900

    駒の動きのテスト記述

--- src/domain/move.test.ts
+++ src/domain/move.test.ts
@@ -1,5 +1,8 @@
 import { describe, expect, it } from 'vitest'
 import * as move from './move'
+import { getEmptyBoard } from './board'
+
+const EMPTY_BOARD = getEmptyBoard()
 
 describe('動きに関するテスト', () => {
   it('moveFrontのテスト', () => {
@@ -47,23 +50,28 @@ describe('動きに関するテスト', () => {
   it('moveLeftBackのテスト', () => {
     expect(move.moveLeftBack(1, { x: 3, y: 3 })).toEqual([{ x: 2, y: 4 }])
     expect(move.moveLeftBack(1, { x: 0, y: 0 })).toEqual([])
-    expect(move.moveLeftBack(2, { x: 0, y: 0 })).toEqual([{ x: 1, y: 1 }])
-    expect(move.moveLeftBack(2, { x: 8, y: 8 })).toEqual([])
+    expect(move.moveLeftBack(2, { x: 0, y: 0 })).toEqual([])
+    expect(move.moveLeftBack(2, { x: 8, y: 8 })).toEqual([{ x: 7, y: 7 }])
   })
   it('moveKeimaのテスト', () => {
-    expect(move.moveKeima(1, { x: 3, y: 3 })).toEqual([{ x: 2, y: 1 }])
+    expect(move.moveKeima(1, { x: 3, y: 3 }).sort()).toEqual(
+      [
+        { x: 2, y: 1 },
+        { x: 4, y: 1 },
+      ].sort(),
+    )
     expect(move.moveKeima(1, { x: 1, y: 0 })).toEqual([])
     expect(move.moveKeima(2, { x: 0, y: 0 })).toEqual([{ x: 1, y: 2 }])
     expect(move.moveKeima(2, { x: 8, y: 8 })).toEqual([])
   })
   it('moveFrontStraightのテスト', () => {
-    expect(move.moveFrontStraight(1, { x: 3, y: 3 })).toEqual([
+    expect(move.moveFrontStraight(1, { x: 3, y: 3 }, EMPTY_BOARD)).toEqual([
       { x: 3, y: 2 },
       { x: 3, y: 1 },
       { x: 3, y: 0 },
     ])
-    expect(move.moveFrontStraight(1, { x: 1, y: 0 })).toEqual([])
-    expect(move.moveFrontStraight(2, { x: 0, y: 0 })).toEqual([
+    expect(move.moveFrontStraight(1, { x: 1, y: 0 }, EMPTY_BOARD)).toEqual([])
+    expect(move.moveFrontStraight(2, { x: 0, y: 0 }, EMPTY_BOARD)).toEqual([
       { x: 0, y: 1 },
       { x: 0, y: 2 },
       { x: 0, y: 3 },
@@ -73,19 +81,28 @@ describe('動きに関するテスト', () => {
       { x: 0, y: 7 },
       { x: 0, y: 8 },
     ])
-    expect(move.moveFrontStraight(2, { x: 8, y: 8 })).toEqual([])
+    expect(move.moveFrontStraight(2, { x: 8, y: 8 }, EMPTY_BOARD)).toEqual([])
   })
   it('moveBackStraightのテスト', () => {
-    expect(move.moveBackStraight(1, { x: 3, y: 3 })).toEqual([
+    expect(move.moveBackStraight(1, { x: 3, y: 3 }, EMPTY_BOARD)).toEqual([
       { x: 3, y: 4 },
       { x: 3, y: 5 },
       { x: 3, y: 6 },
       { x: 3, y: 7 },
       { x: 3, y: 8 },
     ])
-    expect(move.moveBackStraight(1, { x: 1, y: 0 })).toEqual([])
-    expect(move.moveBackStraight(2, { x: 0, y: 0 })).toEqual([])
-    expect(move.moveBackStraight(2, { x: 5, y: 5 })).toEqual([
+    expect(move.moveBackStraight(1, { x: 1, y: 0 }, EMPTY_BOARD)).toEqual([
+      { x: 1, y: 1 },
+      { x: 1, y: 2 },
+      { x: 1, y: 3 },
+      { x: 1, y: 4 },
+      { x: 1, y: 5 },
+      { x: 1, y: 6 },
+      { x: 1, y: 7 },
+      { x: 1, y: 8 },
+    ])
+    expect(move.moveBackStraight(2, { x: 0, y: 0 }, EMPTY_BOARD)).toEqual([])
+    expect(move.moveBackStraight(2, { x: 5, y: 5 }, EMPTY_BOARD)).toEqual([
       { x: 5, y: 4 },
       { x: 5, y: 3 },
       { x: 5, y: 2 },
@@ -94,14 +111,29 @@ describe('動きに関するテスト', () => {
     ])
   })
   it('moveLeftStraightのテスト', () => {
-    expect(move.moveLeftStraight(1, { x: 3, y: 3 })).toEqual([
+    expect(move.moveLeftStraight(1, { x: 3, y: 3 }, EMPTY_BOARD)).toEqual([
       { x: 2, y: 3 },
       { x: 1, y: 3 },
       { x: 0, y: 3 },
     ])
-    expect(move.moveLeftStraight(1, { x: 1, y: 0 })).toEqual([])
-    expect(move.moveLeftStraight(2, { x: 0, y: 0 })).toEqual([])
-    expect(move.moveLeftStraight(2, { x: 5, y: 5 })).toEqual([
+    expect(move.moveLeftStraight(1, { x: 1, y: 0 }, EMPTY_BOARD)).toEqual([{ x: 0, y: 0 }])
+    expect(move.moveLeftStraight(2, { x: 5, y: 0 }, EMPTY_BOARD)).toEqual([
+      { x: 6, y: 0 },
+      { x: 7, y: 0 },
+      { x: 8, y: 0 },
+    ])
+  })
+  it('moveRightStraightのテスト', () => {
+    expect(move.moveRightStraight(1, { x: 3, y: 3 }, EMPTY_BOARD)).toEqual([
+      { x: 4, y: 3 },
+      { x: 5, y: 3 },
+      { x: 6, y: 3 },
+      { x: 7, y: 3 },
+      { x: 8, y: 3 },
+    ])
+    expect(move.moveRightStraight(1, { x: 8, y: 8 }, EMPTY_BOARD)).toEqual([])
+    expect(move.moveRightStraight(2, { x: 0, y: 0 }, EMPTY_BOARD)).toEqual([])
+    expect(move.moveRightStraight(2, { x: 5, y: 5 }, EMPTY_BOARD)).toEqual([
       { x: 4, y: 5 },
       { x: 3, y: 5 },
       { x: 2, y: 5 },
@@ -109,43 +141,24 @@ describe('動きに関するテスト', () => {
       { x: 0, y: 5 },
     ])
   })
-  it('moveRightStraightのテスト', () => {
-    expect(move.moveRightStraight(1, { x: 3, y: 3 })).toEqual([
-      { x: 4, y: 3 },
-      { x: 5, y: 3 },
-      { x: 6, y: 3 },
-      { x: 7, y: 3 },
-      { x: 8, y: 3 },
-    ])
-    expect(move.moveRightStraight(1, { x: 8, y: 8 })).toEqual([])
-    expect(move.moveRightStraight(2, { x: 0, y: 0 })).toEqual([
-      { x: 1, y: 0 },
-      { x: 2, y: 0 },
-      { x: 3, y: 0 },
-      { x: 4, y: 0 },
-      { x: 5, y: 0 },
-      { x: 6, y: 0 },
-      { x: 7, y: 0 },
-      { x: 8, y: 0 },
-    ])
-    expect(move.moveRightStraight(2, { x: 5, y: 5 })).toEqual([])
-  })
   it('moveDiagonalのテスト', () => {
-    expect(move.moveDiagonal(1, { x: 3, y: 3 })).toEqual([
+    expect(move.moveDiagonal(1, { x: 3, y: 3 }, EMPTY_BOARD)).toEqual([
+      { x: 4, y: 4 },
+      { x: 5, y: 5 },
+      { x: 6, y: 6 },
+      { x: 7, y: 7 },
+      { x: 8, y: 8 },
+      { x: 2, y: 4 },
+      { x: 1, y: 5 },
+      { x: 0, y: 6 },
       { x: 4, y: 2 },
       { x: 5, y: 1 },
       { x: 6, y: 0 },
       { x: 2, y: 2 },
       { x: 1, y: 1 },
       { x: 0, y: 0 },
-      { x: 4, y: 4 },
-      { x: 5, y: 5 },
-      { x: 6, y: 6 },
-      { x: 7, y: 7 },
-      { x: 2, y: 4 },
     ])
-    expect(move.moveDiagonal(1, { x: 1, y: 0 })).toEqual([])
-    expect(move.moveDiagonal(2, { x: 0, y: 0 })).toEqual([
+    expect(move.moveDiagonal(2, { x: 0, y: 0 }, EMPTY_BOARD)).toEqual([
       { x: 1, y: 1 },
       { x: 2, y: 2 },
       { x: 3, y: 3 },
@@ -154,17 +167,6 @@ describe('動きに関するテスト', () => {
       { x: 6, y: 6 },
       { x: 7, y: 7 },
       { x: 8, y: 8 },
-    ])
-    expect(move.moveDiagonal(2, { x: 5, y: 5 })).toEqual([
-      { x: 6, y: 6 },
-      { x: 7, y: 7 },
-      { x: 8, y: 8 },
-      { x: 4, y: 6 },
-      { x: 3, y: 7 },
-      { x: 2, y: 8 },
-      { x: 6, y: 4 },
-      { x: 7, y: 3 },
-      { x: 8, y: 2 },
     ])
   })
 })
--- src/domain/move.ts
+++ src/domain/move.ts
@@ -1,4 +1,4 @@
-import { Coordinate, Turn } from '.'
+import { Board, Coordinate, Turn } from '.'
 
 // 前に進む
 export const moveFront = (turn: Turn, { x, y }: Coordinate): Coordinate[] => {
@@ -80,10 +80,10 @@ export const moveLeftFront = (turn: Turn, { x, y }: Coordinate): Coordinate[] =>
 // 左斜め後ろ
 export const moveLeftBack = (turn: Turn, { x, y }: Coordinate): Coordinate[] => {
   if (turn === 1) {
-    if (x > 0 && y > 0) return [{ x: x - 1, y: y - 1 }]
+    if (x > 0 && y < 8) return [{ x: x - 1, y: y + 1 }]
   }
   if (turn === 2) {
-    if (x < 8 && y < 8) return [{ x: x + 1, y: y + 1 }]
+    if (x > 0 && y > 0) return [{ x: x - 1, y: y - 1 }]
   }
   return []
 }
@@ -92,97 +92,133 @@ export const moveLeftBack = (turn: Turn, { x, y }: Coordinate): Coordinate[] => 
 export const moveKeima = (turn: Turn, { x, y }: Coordinate): Coordinate[] => {
   const coordinates: Coordinate[] = []
   if (turn === 1) {
-    if (x > 0 && y < 7) coordinates.push({ x: x - 1, y: y + 2 })
-    if (x < 8 && y < 7) coordinates.push({ x: x + 1, y: y + 2 })
-  }
-  if (turn === 2) {
     if (x > 0 && y > 1) coordinates.push({ x: x - 1, y: y - 2 })
     if (x < 8 && y > 1) coordinates.push({ x: x + 1, y: y - 2 })
   }
+  if (turn === 2) {
+    if (x > 0 && y < 7) coordinates.push({ x: x - 1, y: y + 2 })
+    if (x < 8 && y < 7) coordinates.push({ x: x + 1, y: y + 2 })
+  }
   return coordinates
 }
 
-// まっすぐ前に進む動き
-export const moveFrontStraight = (turn: Turn, { x, y }: Coordinate): Coordinate[] => {
+// まっすぐ後ろに進む動き
+export const moveBackStraight = (turn: Turn, { x, y }: Coordinate, board: Board): Coordinate[] => {
   const coordinates: Coordinate[] = []
   if (turn === 1) {
     for (let i = y + 1; i < 9; i++) {
       coordinates.push({ x, y: i })
+      if (board[i][x].pieceState) break
     }
   }
   if (turn === 2) {
     for (let i = y - 1; i >= 0; i--) {
       coordinates.push({ x, y: i })
+      if (board[i][x].pieceState) break
     }
   }
   return coordinates
 }
 
-// まっすぐ後ろに進む動き(飛車、龍)
-export const moveBackStraight = (turn: Turn, { x, y }: Coordinate): Coordinate[] => {
+// まっすぐ前に進む動き(飛車、龍)
+export const moveFrontStraight = (turn: Turn, { x, y }: Coordinate, board: Board): Coordinate[] => {
   const coordinates: Coordinate[] = []
   if (turn === 1) {
     for (let i = y - 1; i >= 0; i--) {
       coordinates.push({ x, y: i })
+      if (board[i][x].pieceState) break
     }
   }
   if (turn === 2) {
     for (let i = y + 1; i < 9; i++) {
       coordinates.push({ x, y: i })
+      if (board[i][x].pieceState) break
     }
   }
   return coordinates
 }
 
 // まっすぐ右に進む動き(飛車、龍)
-export const moveRightStraight = (turn: Turn, { x, y }: Coordinate): Coordinate[] => {
+export const moveRightStraight = (turn: Turn, { x, y }: Coordinate, board: Board): Coordinate[] => {
   const coordinates: Coordinate[] = []
   if (turn === 1) {
     for (let i = x + 1; i < 9; i++) {
       coordinates.push({ x: i, y })
+      if (board[y][i].pieceState) break
     }
   }
   if (turn === 2) {
     for (let i = x - 1; i >= 0; i--) {
       coordinates.push({ x: i, y })
+      if (board[y][i].pieceState) break
     }
   }
   return coordinates
 }
 
 // まっすぐ左に進む動き(飛車、龍)
-export const moveLeftStraight = (turn: Turn, { x, y }: Coordinate): Coordinate[] => {
+export const moveLeftStraight = (turn: Turn, { x, y }: Coordinate, board: Board): Coordinate[] => {
   const coordinates: Coordinate[] = []
   if (turn === 1) {
     for (let i = x - 1; i >= 0; i--) {
       coordinates.push({ x: i, y })
+      if (board[y][i].pieceState) break
     }
   }
   if (turn === 2) {
     for (let i = x + 1; i < 9; i++) {
       coordinates.push({ x: i, y })
+      if (board[y][i].pieceState) break
     }
   }
   return coordinates
 }
 
 // 斜めに進む動き(角、馬)
-export const moveDiagonal = (turn: Turn, { x, y }: Coordinate): Coordinate[] => {
+export const moveDiagonal = (turn: Turn, { x, y }: Coordinate, board: Board): Coordinate[] => {
   const coordinates: Coordinate[] = []
   if (turn === 1) {
     for (let i = 1; i < 9; i++) {
-      if (x + i < 9 && y + i < 9) coordinates.push({ x: x + i, y: y + i })
-      if (x - i >= 0 && y + i < 9) coordinates.push({ x: x - i, y: y + i })
-      if (x + i < 9 && y - i >= 0) coordinates.push({ x: x + i, y: y - i })
-      if (x - i >= 0 && y - i >= 0) coordinates.push({ x: x - i, y: y - i })
+      if (x + i > 8 || y + i > 8) break
+      coordinates.push({ x: x + i, y: y + i })
+      if (board[y + i][x + i].pieceState) break
+    }
+    for (let i = 1; i < 9; i++) {
+      if (x - i < 0 || y + i > 8) break
+      coordinates.push({ x: x - i, y: y + i })
+      if (board[y + i][x - i].pieceState) break
+    }
+    for (let i = 1; i < 9; i++) {
+      if (x + i > 8 || y - i < 0) break
+      coordinates.push({ x: x + i, y: y - i })
+      if (board[y - i][x + i].pieceState) break
+    }
+    for (let i = 1; i < 9; i++) {
+      if (x - i < 0 || y - i < 0) break
+      coordinates.push({ x: x - i, y: y - i })
+      if (board[y - i][x - i].pieceState) break
     }
   }
   if (turn === 2) {
     for (let i = 1; i < 9; i++) {
-      if (x + i < 9 && y + i < 9) coordinates.push({ x: x + i, y: y + i })
-      if (x - i >= 0 && y + i < 9) coordinates.push({ x: x - i, y: y + i })
-      if (x + i < 9 && y - i >= 0) coordinates.push({ x: x + i, y: y - i })
-      if (x - i >= 0 && y - i >= 0) coordinates.push({ x: x - i, y: y - i })
+      if (x + i > 8 || y + i > 8) break
+      coordinates.push({ x: x + i, y: y + i })
+      if (board[y + i][x + i].pieceState) break
+    }
+    for (let i = 1; i < 9; i++) {
+      if (x - i < 0 || y + i > 8) break
+      coordinates.push({ x: x - i, y: y + i })
+      if (board[y + i][x - i].pieceState) break
+    }
+    for (let i = 1; i < 9; i++) {
+      if (x + i > 9 || y - i < 0) break
+      coordinates.push({ x: x + i, y: y - i })
+      if (board[y - i][x + i].pieceState) break
+    }
+    for (let i = 1; i < 9; i++) {
+      if (x - i < 0 || y - i < 0) break
+      coordinates.push({ x: x - i, y: y - i })
+      if (board[y - i][x - i].pieceState) break
     }
   }
   return coordinates
